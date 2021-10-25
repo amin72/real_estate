0b73da5209434ebe1b3ef85ef79f68013cbd6ce7
Amin <asaa47721@gmail.com>
2021-10-25 17:22:20 +0330

    Add Listing model. define listings migration

---
+++ app/Models/Listing.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+
+    public $fillable = ['title', 'address', 'city', 'zipcode',
+        'description', 'price', 'betrooms', 'has_store', 'has_garage',
+        'sqft', 'is_published', 'image', 'image_1', 'image_2', 'image_3',
+        'image_4', 'image_5', 'image_6'
+    ];
+}
---
+++ database/migrations/2021_10_25_103310_create_listings_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('title', 200);
+            $table->string('address', 300);
+            $table->string('city', 100);
+            $table->string('zipcode', 20);
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('price');
+            $table->unsignedTinyInteger('betrooms');
+            $table->boolean('has_store');
+            $table->boolean('has_garage');
+            $table->unsignedInteger('sqft');
+            $table->boolean('is_published')->default(false);
+
+            $table->string('image');
+            $table->string('image_1')->nullable();
+            $table->string('image_2')->nullable();
+            $table->string('image_3')->nullable();
+            $table->string('image_4')->nullable();
+            $table->string('image_5')->nullable();
+            $table->string('image_6')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+}
