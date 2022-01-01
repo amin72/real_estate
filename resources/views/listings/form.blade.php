cfbad1d72d5d35712d84cbf4c96ea9c73ecb5a18
Amin <asaa47721@gmail.com>
2022-01-01 13:26:56 +0330

    add more info to fields labels

--- resources/views/listings/form.blade.php
+++ resources/views/listings/form.blade.php
@@ -168,7 +168,7 @@
 
         <!-- Bedrooms -->
         <div class="mt-4">
-          <label for="bedrooms">تعداد اتاق خواب <span class="text-3xl text-black">*</span></label>
+          <label for="bedrooms">تعداد اتاق خواب (فقط عدد وارد شود)<span class="text-3xl text-black">*</span></label>
           <input
             type="text"
             name="bedrooms"
@@ -180,7 +180,7 @@
 
         <!-- Area -->
         <div class="mt-4">
-          <label for="area">متراژ <span class="text-3xl text-black">*</span></label>
+          <label for="area">متراژ (فقط عدد وارد شود)<span class="text-3xl text-black">*</span></label>
           <input
             type="text"
             name="area"
@@ -351,7 +351,7 @@
               <path d="M1344 864q0-14-9-23l-352-352q-9-9-23-9t-23 9l-351 351q-10 12-10 24 0 14 9 23t23 9h224v352q0 13 9.5 22.5t22.5 9.5h192q13 0 22.5-9.5t9.5-22.5v-352h224q13 0 22.5-9.5t9.5-22.5zm640 288q0 159-112.5 271.5t-271.5 112.5h-1088q-185 0-316.5-131.5t-131.5-316.5q0-130 70-240t188-165q-2-30-2-43 0-212 150-362t362-150q156 0 285.5 87t188.5 231q71-62 166-62 106 0 181 75t75 181q0 76-41 138 130 31 213.5 135.5t83.5 238.5z"/>
             </svg>
             
-            <span id="image_name" class="mt-2 text-sm leading-normal">تصویر اصلی</span>
+            <span id="image_name" class="mt-2 text-sm leading-normal">تصویر اصلی <span class="text-3xl text-black">*</span></span>
             <input id="image" type="file" name="image" class="hidden" accept="image/*" />
           </label>
 
